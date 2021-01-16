0fa8dca3b2ab7a766eb572354fcd080606a7ec03
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-01-15 23:15:28 -0500

    Writing out the parent class of Employee with its subclasses and their methods

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,71 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        if (!name) {
+            throw new Error("You are missing the name.");
+          }
+          if (!id) {
+            throw new Error("You are missing the id.");
+          }
+          if (!email) {
+            throw new Error("You are missing the email.");
+          }
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+        console.log(`Name: ${this.name}`);
+    };
+
+    getId() {
+        console.log(`Id: ${this.id}`)
+    };
+
+    getEmail() {
+        console.log(`Email: ${this.email}`)
+    };
+
+    getRole() {
+        // write code here. not too sure
+    };
+};
+
+class Employee extends Manager {
+    constuctor(officeNumber) {
+        this.officeNumber = officeNumber;
+        super(); // calls Employee's class
+    };
+
+    getRole() {
+        // write code
+    };
+};
+
+class Employee extends Engineer {
+    constructor(github) {
+        this.github = github;
+        super();
+
+    };
+
+    getRole() {
+        //write code
+    };
+};
+
+class Employee extends Intern {
+    constructor(school) {
+        this.school = school;
+        super();
+    };
+
+    getSchool(){
+        // write code
+    };
+
+    getRole() {
+        //write code
+    };
+};
