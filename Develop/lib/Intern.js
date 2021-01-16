558de48fa021961cf8e1b91331a8d92132f8b8ce
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-01-16 00:04:27 -0500

    Added each subclass into their respective lib file and called the Employee parent through require in each file

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Employee extends Intern {
+    constructor(school, name, id, email) {
+        super(name, id, email);
+        this.school = school;
+        
+    };
+
+    getSchool(school){
+        this.school = school;
+    };
+
+    getRole() {
+        //write code
+    };
+};
+
+// still need to add some code here. Researching
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Employee extends Manager {
+    constuctor(officeNumber, name, id, email) {
+        super(name, id, email); // calls Employee's class
+        this.officeNumber = officeNumber;
+    };
+
+    getRole() {
+        // write code
+    };
+};
+
+// need more code here. TBD
