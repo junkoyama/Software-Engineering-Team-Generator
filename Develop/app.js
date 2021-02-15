c1893950819c148fcc8187c4b6c2e3a4ed72776e
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-02-15 01:01:04 -0500

    Added option for user to add another employee to add to template

--- Develop/app.js
+++ Develop/app.js
@@ -12,86 +12,146 @@ const render = require("./lib/htmlRenderer");
 
 const listEngTeam = [];
 
-const promptEmployeeQs = () => 
+// const managerLogIn = () =>
+// inquirer.prompt([
+//       {
+//       type: "input",
+//       name: "manager_name",
+//       message: "Hello, Manager. Please enter your name to log in",
+//     },
+// ]);
+
+const promptEmployeeQs = () =>
   inquirer.prompt([
     {
       type: "input",
       name: "manager_name",
-      message: "Hello, Manager. Please enter your name to log in"
+      message:
+        "Hello, Manager. Please enter your name to log in and add team member",
     },
-  {
-    type: "list",
-    name: "role",
-    message: "Which role would you like to add to your team?",
-    choices: ["Engineer", "Intern"]
-  },
-  {
-    type: "input",
-    name: "name",
-    message: "Please enter their name",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "Please enter their id"
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Please enter their email"
-  },
-  {
-    type: "input",
-    name: "office",
-    message: "Please enter the employee's office phone number",
-    when: (answer) => answer.manager_name === true
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "Please enter the employee's github",
-    when: (answer) => answer.role === "Engineer"
-  },
-  {
-    type: "input",
-    name: "school",
-    message: "Please enter the name of the employee's school",
-    when: (answer) => answer.role === "Intern"
-  },
-  {
-    type: "confirm",
-    name: "addAnotherEmployee",
-    message: "Would you like to add another employee?"
-  }
-]);
+    {
+      type: "list",
+      name: "role",
+      message: "Which role would you like to add to your team?",
+      choices: ["Engineer", "Intern"],
+    },
+    {
+      type: "input",
+      name: "name",
+      message: "Please enter their name",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Please enter their id",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter their email",
+    },
+    {
+      type: "input",
+      name: "office",
+      message: "Please enter the employee's office phone number",
+      when: (answer) => answer.manager_name === true,
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Please enter the employee's github",
+      when: (answer) => answer.role === "Engineer",
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "Please enter the name of the employee's school",
+      when: (answer) => answer.role === "Intern",
+    },
+    {
+      type: "confirm",
+      name: "addAnotherEmployee",
+      message: "Would you like to add another employee?",
+    },
+  ]);
 
 const init = () => {
-  promptEmployeeQs()
-  .then((answer) => {
-  if (answer.role === "Manager"){
+  promptEmployeeQs().then((answer) => {
+    if (answer.role === "Manager") {
+      const mgrRole = new Manager(
+        answer.name,
+        answer.id,
+        answer.email,
+        answer.officeNumber
+      );
 
-    const mgrRole = new Manager (answer.name, answer.id, answer.email, answer.officeNumber);
+      listEngTeam.push(mgrRole);
+    } else if (answer.role === "Engineer") {
+      const engRole = new Engineer(
+        answer.name,
+        answer.id,
+        answer.email,
+        answer.github
+      );
+
+      listEngTeam.push(engRole);
+    } else if (answer.role === "Intern") {
+      const internRole = new Intern(
+        answer.name,
+        answer.id,
+        answer.email,
+        answer.school
+      );
+
+      listEngTeam.push(internRole);
+    }
+    generateHtml();
+
+    if (answer.addAnotherEmployee) {
+      promptEmployeeQs()
+      .then((answer) => {
+        if (answer.role === "Manager") {
+          const mgrRole = new Manager(
+            answer.name,
+            answer.id,
+            answer.email,
+            answer.officeNumber
+          );
     
-    listEngTeam.push(mgrRole);
-
-  } else if (answer.role === "Engineer"){
-
-    const engRole = new Engineer (answer.name, answer.id, answer.email, answer.github);
+          listEngTeam.push(mgrRole);
+        } else if (answer.role === "Engineer") {
+          const engRole = new Engineer(
+            answer.name,
+            answer.id,
+            answer.email,
+            answer.github
+          );
     
-    listEngTeam.push(engRole);
-
-  } else if (answer.role === "Intern") {
-
-    const internRole = new Intern (answer.name, answer.id, answer.email, answer.school)
-
-    listEngTeam.push(internRole);
-
-  } const html = render(listEngTeam);
-      fs.writeFile(outputPath, html, (error) => {
-        if (error) {
-          throw error
+          listEngTeam.push(engRole);
+        } else if (answer.role === "Intern") {
+          const internRole = new Intern(
+            answer.name,
+            answer.id,
+            answer.email,
+            answer.school
+          );
+    
+          listEngTeam.push(internRole);
         }
-      });
+        generateHtml();
+      })
+    } else {
+      console.log("Success! You may now view your team template.");
+    };
+  });
+};
+
+const generateHtml = () => {
+  const html = render(listEngTeam);
+  fs.writeFile(outputPath, html, (error) => {
+    if (error) {
+      throw error;
+    }
   });
 };
 
